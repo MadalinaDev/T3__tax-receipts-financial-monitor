609d59acf0152e668d1d3b316e6dacafd4d9152b
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2025-05-19 14:46:33 +0300

    first commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["tax-receipts-financial-monitor__t3_*"],
+} satisfies Config;
