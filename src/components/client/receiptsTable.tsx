5b3da6f0f9aac52af11a1e7828d48aeebeb3bb7d
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2025-08-10 00:41:38 +0300

    added filters for receipts

--- src/components/client/receiptsTable.tsx
+++ src/components/client/receiptsTable.tsx
@@ -1,9 +1,5 @@
 "use client";
-import {
-  Card,
-  CardHeader,
-  CardContent,
-} from "../ui/card";
+import { Card, CardHeader, CardContent, CardFooter } from "../ui/card";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Separator } from "../ui/separator";
@@ -13,24 +9,42 @@ import {
   ListFilter,
   MapPinIcon,
   Calendar,
+  MoveLeft,
+  MoveRight,
 } from "lucide-react";
 import TimeAgo from "react-timeago";
+import { DatePicker } from "../ui/date-picker";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "../ui/select";
 
 const ReceiptsTable = () => {
   return (
-    <div className="my-8 grid w-full grid-cols-4 gap-x-8">
+    <div className="my-8 grid w-full grid-cols-7 gap-x-8">
       <ReceiptsFilters />
-      <div className="col-span-3">
+      <div className="col-span-5">
         <div className="relative">
           <Search className="absolute top-3 left-3 size-4" />
           <Input
             placeholder="Search receipts..."
-            // value={searchTerm}
-            // onChange={(e) => setSearchTerm(e.target.value)}
             className="pl-9"
           />
         </div>
         <ReceiptsGrid />
+        {/* pagination section */}
+        <div className="flex flex-row items-center justify-center gap-x-3">
+          <Button variant="outline" className="text-muted-foreground font-normal">
+            <MoveLeft /> Previous
+          </Button>
+          <Button variant="default">1</Button>
+          <Button variant="outline" className="text-muted-foreground font-normal">
+            Next <MoveRight />{" "}
+          </Button>
+        </div>
       </div>
     </div>
   );
@@ -40,7 +54,7 @@ export default ReceiptsTable;
 
 const ReceiptsFilters = () => {
   return (
-    <Card className="col-span-1">
+    <Card className="col-span-2 self-start">
       <CardHeader className="flex items-center justify-between">
         <div className="flex flex-row items-center gap-x-2">
           <ListFilter className="size-5" /> Filters
@@ -49,7 +63,57 @@ const ReceiptsFilters = () => {
           Clear All
         </Button>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        <div className="my-6 text-xs font-semibold">
+          <div className="mb-2">Date Range</div>
+          <div className="flex w-full flex-row gap-2 font-semibold">
+            <div className="flex-1 text-xs">
+              From
+              <DatePicker />
+            </div>
+            <div className="flex-1 text-xs">
+              To
+              <DatePicker />
+            </div>
+          </div>
+        </div>
+        <div className="my-6 text-xs font-semibold">
+          <div className="mb-2">Amount Range</div>
+          <div className="flex flex-row gap-2 font-semibold">
+            <div>
+              Min (MDL)
+              <Input type="number" placeholder="0.00" className="font-normal" />
+            </div>
+            <div className="text-xs">
+              Max(MDL)
+              <Input
+                type="number"
+                placeholder="999.99"
+                className="font-normal"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="my-6 text-xs">
+          <div className="mb-2 font-semibold">Sort By</div>
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Date (Newest)" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="newest">Date (Newest)</SelectItem>
+              <SelectItem value="oldest">Date (Oldest)</SelectItem>
+              <SelectItem value="high">Amount (High to Low)</SelectItem>
+              <SelectItem value="low">Amount (Low to High)</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button variant="outline" size="sm" className="w-full">
+          Apply
+        </Button>
+      </CardFooter>
     </Card>
   );
 };
@@ -215,7 +279,8 @@ const ReceiptsGrid = () => {
             <div className="mb-2 flex flex-row items-center text-xs">
               <Calendar className="mr-2 size-4" />{" "}
               <div className="text-muted-foreground">
-                <TimeAgo date={receipt.createdAt} />{" "} ({receipt.createdAt.toLocaleDateString()})
+                <TimeAgo date={receipt.createdAt} /> (
+                {receipt.createdAt.toLocaleDateString()})
               </div>
             </div>
             <Separator orientation="horizontal" className="my-1" />
@@ -223,9 +288,9 @@ const ReceiptsGrid = () => {
             <div className="my-3">
               {receipt.products.map((product) => (
                 <div key={product.id} className="text-sm">
-                  <div className="flex justify-between items-center">
+                  <div className="flex items-center justify-between">
                     {product.name}
-                    <span className="text-xs text-muted-foreground">
+                    <span className="text-muted-foreground text-xs">
                       {product.quantity} x {product.unitPrice}{" "}
                     </span>
                   </div>
