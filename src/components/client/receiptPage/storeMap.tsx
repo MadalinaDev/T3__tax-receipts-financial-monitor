5ac3948a7ce93c05bc66e2149051bf49c1b69fb5
Madalina Chirpicinic <madalina.chirpicinic@gmail.com>
2025-10-02 15:19:10 +0300

    call to geocoder for getting and dispalying location coordinates

--- src/components/client/receiptPage/storeMap.tsx
+++ src/components/client/receiptPage/storeMap.tsx
@@ -2,6 +2,14 @@
 
 import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
+import { useState, useEffect } from "react";
+import PageLoader from "~/components/layout/pageLoader";
+
+interface GeocodeResponseType {
+  response?: {
+    bbox?: [number, number, number, number];
+  };
+}
 
 const MapContainer = dynamic(
   () => import("react-leaflet").then((mod) => mod.MapContainer),
@@ -16,22 +24,51 @@ const Marker = dynamic(
   { ssr: false },
 );
 
-
 export default function StoreMap({ address }: { address: string }) {
+  const [position, setPosition] = useState<[number, number]>([
+    47.025604, 28.830367,
+  ]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  console.log("Store address: ", address);
+  useEffect(() => {
+    const getGeocode = async () => {
+      const response = await fetch("/api/geocode", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          address,
+        }),
+      });
+      const data = (await response.json()) as GeocodeResponseType;
+      const coords = data?.response?.bbox;
+      // for some locations, the geocode is not found by the API
+      // to do: show appropiate fallback
+      if (coords && coords.length > 3) setPosition([coords[3], coords[0]]);
+      setLoading(false);
+    };
+
+    void getGeocode();
+  }, []);
 
   return (
-    <MapContainer
-      center={[47.0105, 28.8638]}
-      zoom={13}
-      style={{ height: "50vh", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={[51.505, -0.09] as [number, number]} />
-    </MapContainer>
+    <>
+      {loading ? (
+        <PageLoader />
+      ) : (
+        <MapContainer
+          center={position}
+          zoom={13}
+          style={{ height: "60vh", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[51.505, -0.09] as [number, number]} />
+        </MapContainer>
+      )}
+    </>
   );
 }
--- src/components/layout/pageLoader.tsx
+++ src/components/layout/pageLoader.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from "lucide-react";
 
 const PageLoader = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-[20vh] flex justify-center items-center">
       <Loader2 className="text-navy-blue animate-spi\n" />
     </div>
   );
